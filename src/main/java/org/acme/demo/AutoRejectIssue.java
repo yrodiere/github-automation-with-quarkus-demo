858ecec87f14fcce3995c0ab0688df0350b9565c
Yoann Rodière <yoann@hibernate.org>
2022-06-01 14:10:44 +0200

    Add per-repository configuration file

--- src/main/java/org/acme/demo/AutoRejectIssue.java
+++ src/main/java/org/acme/demo/AutoRejectIssue.java
@@ -3,16 +3,20 @@ package org.acme.demo;
 import java.io.IOException;
 import java.util.Locale;
 
+import io.quarkiverse.githubapp.ConfigFile;
 import io.quarkiverse.githubapp.event.Issue;
 import org.kohsuke.github.GHEventPayload;
 
 public class AutoRejectIssue {
-    void onIssueOpened(@Issue.Opened GHEventPayload.Issue payload) throws IOException {
+    void onIssueOpened(@Issue.Opened GHEventPayload.Issue payload,
+                        @ConfigFile("demoapp.yaml") RepositoryConfig repoConfig) throws IOException {
         var issue = payload.getIssue();
-        if (issue.getTitle().toLowerCase(Locale.ROOT).contains("osgi")
-                || issue.getBody() != null && issue.getBody().toLowerCase(Locale.ROOT).contains("osgi")) {
-            payload.getIssue().comment("Sorry, OSGi integration is not supported at the moment.");
-            payload.getIssue().close();
+        for (String keyword : repoConfig.autoreject) {
+            if (issue.getTitle().toLowerCase(Locale.ROOT).contains(keyword)
+                    || issue.getBody() != null && issue.getBody().toLowerCase(Locale.ROOT).contains(keyword)) {
+                payload.getIssue().comment("Sorry, " + keyword + " integration is not supported at the moment.");
+                payload.getIssue().close();
+            }
         }
     }
 }
---
+++ src/main/java/org/acme/demo/RepositoryConfig.java
@@ -0,0 +1,8 @@
+package org.acme.demo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RepositoryConfig {
+	public Set<String> autoreject = new HashSet<>();
+}
